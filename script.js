df95235b643569f9c1efcf2ab1c962ead261e75b
Ankit maharana <91303709+ankitmaharana@users.noreply.github.com>
2025-06-21 12:02:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const currencyFirstEl = document.getElementById("currency-first");
+const worthFirstEl = document.getElementById("worth-first");
+const currencySecondtEl = document.getElementById("currency-second");
+const worthSecondtEl = document.getElementById("worth-second");
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+
+updateRate()
+
+
+function updateRate() {
+    fetch(`https://v6.exchangerate-api.com/v6/99b221ad136cd5836a3c87ff/latest/${currencyFirstEl.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data);
+            const rate = data.conversion_rates[currencySecondtEl.value];
+            console.log(rate);
+            exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondtEl.value}`;
+
+            worthSecondtEl.value = (worthFirstEl.value * rate).toFixed(2)
+
+        }
+        );
+}
+
+
+
+currencyFirstEl.addEventListener("change", updateRate);
+currencySecondtEl.addEventListener("change", updateRate);
+
+worthFirstEl.addEventListener("input", updateRate);
+
